bcef73fa29bbc50fad1f58c99e47eb2a49799a55
sooj <soojksh@gmail.com>
2024-04-10 23:24:03 +0530

    Initial commit - CRUD API set up

---
+++ api/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Student
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'grade', 'roll', 'city')
+    list_display_links = ('name', )
+# Register your models here.
+admin.site.register(Student, StudentAdmin)
