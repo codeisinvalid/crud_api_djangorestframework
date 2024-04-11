623b3a145e7cc8f5c09544543a998e2d514ee946
sooj <soojksh@gmail.com>
2024-04-11 18:52:52 +0530

    get, post and put dont

--- api/serializers.py
+++ api/serializers.py
@@ -2,11 +2,22 @@ from rest_framework import serializers
 from . models import Student
 
 class StudentSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
     name = serializers.CharField(max_length=100)
     roll = serializers.IntegerField()
     grade = serializers.CharField(max_length=100)
     city = serializers.CharField(max_length= 100)
+
+    def create(self, validated_data):
+        return Student.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.roll = validated_data.get('roll', instance.roll)
+        instance.grade = validated_data.get('grade', instance.grade)
+        instance.city = validated_data.get('city', instance.city)
+        instance.save()
+        return instance
+    
 
     
 
--- api/views.py
+++ api/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+import io
+from rest_framework.parsers import JSONParser
+from . models import Student
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+def student_api(request):
+    if request.method == "GET":
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get('id', None)
+
+        if id is not None:
+            student = Student.objects.get(id = id)
+            serializer = StudentSerializer(student)
+            json_data = JSONRenderer().render(serializer.data)
+            return HttpResponse(json_data, content_type= 'application/json')
+        
+        student = Student.objects.all()
+        serializer = StudentSerializer(student, many=True)
+        json_data = JSONRenderer().render(serializer.data)
+        return HttpResponse(json_data, content_type = 'application/json')
+    
+    if request.method == "POST":
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        serializer = StudentSerializer(data = pythondata)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg':'data created'}
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data, content_type = 'application/json')
+        json_error = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_error, content_type = 'application/json')
+    
+    if request.method == "PUT":
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get('id')
+        student = Student.objects.get(id = id)
+        serializer = StudentSerializer(student, data = pythondata, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg':'data updated'}
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data, content_type = 'application/json')
+        json_error = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_error, content_type = 'application/json')
+
+
+
+
